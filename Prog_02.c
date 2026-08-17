0696730010c1a7f4140b32c514e773cf26e2c97f
Everaj <varinder.sgc.er@gmail.com>
2026-08-17 13:22:05 +0530

    Syllabus Program

---
+++ Prog_02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    //Method no 1 Without using scanf() function.
+    int a=10,b=50;
+    printf("Sum of a and b is: %d\n",a+b);
+
+    //Method no 2 Using scanf() function.
+    int x,y;
+    printf("Enter Value of x: ");
+    scanf("%d",&x);
+    printf("Enter Value of y: ");
+    scanf("%d",&y);
+    printf("X = %d\ny = %d\nSum of x and y is: %d",x,y,x+y);
+}
