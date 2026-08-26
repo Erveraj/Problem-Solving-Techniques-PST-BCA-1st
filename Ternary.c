81077ddef7eaa220094b99b2d93f25af1da2b901
Everaj <varinder.sgc.er@gmail.com>
2026-08-26 16:05:54 +0530

    Use Ternary Operator

---
+++ Ternary.c
@@ -0,0 +1,10 @@
+//Program No 05: WRITE A PROGRAM to find the largest of two numbers using ternary operator.
+#include <stdio.h>
+
+int main()
+{
+    int a=105, b=100;
+    printf("%d", (a>b)? a:b);
+
+    return 0;
+}
